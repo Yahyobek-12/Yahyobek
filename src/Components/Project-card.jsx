bde0e9cc158199b687855039882b96f98145e664
Yahyobek <caisophiya@gmail.com>
2024-03-29 06:57:53 +0500

    Portfolio

--- src/Components/Project-card.jsx
+++ src/Components/Project-card.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { CiShare1 } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
-import { useEffect } from 'react';
+import Taskmate from '../Images/taskmate.jpg';
+import Notepad from '../Images/notepad.jpg';
+import Colorspy from '../Images/colorspy.jpg';
+import Worldpin from '../Images/worldpin.jpg';
+import NewTube from '../Images/newtube.jpg';
+import Gitsearcher from '../Images/github.jpg';
+
 
 const ProjectCard = () => {
 
@@ -12,6 +19,7 @@ const ProjectCard = () => {
   return (
     <div className="project-cards animate__animated animate__fadeIn">
       <div className="pr-card">
+        <img src={Taskmate} alt="" />
         <h2>TaskMate</h2>
         <h3><span>Technologies</span>: React-Js || Mui || Animate-Css || React-Router || React-Toastify || React-Icons || Clipboard-Copy</h3>
         <Link to='https://task-mate-ncy.vercel.app/' target='_blank'><CiShare1 className='a-icon' /></Link>
@@ -19,6 +27,7 @@ const ProjectCard = () => {
       </div>
 
       <div className="pr-card">
+        <img src={Notepad} alt="" />
         <h2>Notepad</h2>
         <h3><span>Technologies</span>: Html || Css || Javascript || Sweet-Alert</h3>
         <Link to='https://notepad-beta-one.vercel.app/' target='_blank'><CiShare1 className='a-icon' /></Link>
@@ -26,6 +35,7 @@ const ProjectCard = () => {
       </div>
 
       <div className="pr-card">
+        <img src={Colorspy} alt="" />
         <h2>ColorSpy</h2>
         <h3><span>Technologies</span>: Html || Css || Javascript</h3>
         <Link to='https://new-color-lilac.vercel.app/' target='_blank'><CiShare1 className='a-icon' /></Link>
@@ -33,6 +43,7 @@ const ProjectCard = () => {
       </div>
 
       <div className="pr-card">
+        <img src={Worldpin} alt="" />
         <h2>WorldPin</h2>
         <h3><span>Technologies</span>: React-Js || React-Icons || React-Router || React-Count-Up</h3>
         <Link to='https://worldpin-countries.vercel.app/' target='_blank'><CiShare1 className='a-icon' /></Link>
@@ -40,6 +51,7 @@ const ProjectCard = () => {
       </div>
 
       <div className="pr-card">
+        <img src={Gitsearcher} alt="" />
         <h2>GitSearcher</h2>
         <h3><span>Technologies</span>: React-Js || Animate-Css || React-Router || React-Icons</h3>
         <Link to='https://git-users-two.vercel.app/' target='_blank'><CiShare1 className='a-icon' /></Link>
@@ -47,6 +59,7 @@ const ProjectCard = () => {
       </div>
 
       <div className="pr-card">
+        <img src={NewTube} alt="" />
         <h2>New-Tube</h2>
         <h3><span>Technologies</span>: React-Js || Mui || Animate-Css || React-Router || React-Toastify || React-Icons || Clipboard-Copy || Swiper || React Player</h3>
         <Link to='https://new-tube-nwt.vercel.app/' target='_blank'><CiShare1 className='a-icon' /></Link>
