5dc37263007640d4d8e472a757db628134002fa8
Yahyobek <caisophiya@gmail.com>
2024-04-01 16:32:59 +0500

    Update Blog Component

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -1,10 +1,20 @@
 import '../Styles/Blog.css';
+import logo from '../Images/team.jfif';
+import { MdLocationOn  } from "react-icons/md";
 
 const Blogs = () => {
   return (
     <div className='blogs'>
       <div className="blogs-card">
-        
+        <div className="flex-location">
+          <MdLocationOn  className='location-icon' />
+          <p>California</p>
+        </div>
+        <img className='blog-image' src={logo} alt="" />
+        <div className="flex-content">
+          <h3 className='blog-title'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, voluptatibus.</h3>
+        </div>
+        <p className='date'>4/1/2024</p>
       </div>
     </div>
   )
